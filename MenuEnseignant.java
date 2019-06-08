6acbb43250d3d7bef6fdf00d083df3af4b0bfd34
evadreyfus <36441413+evadreyfus@users.noreply.github.com>
2019-06-08 22:37:21 +0200

    Vue Enseignant

---
+++ MenuEnseignant.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vue;
+
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import modele.Eleve;
+import modele.Personne;
+
+
+/**
+ *
+ * @author evadr
+ */
+public class MenuEnseignant extends JFrame{
+    public MenuEnseignant(Personne enseignantDAO){
+        Personne enseignant = new Eleve(enseignantDAO.getPk_id(),enseignantDAO.getNom(),enseignantDAO.getPrenom(),enseignantDAO.getType());
+        
+        this.setTitle("Gestion école - Enseignant");
+        this.setSize(600,400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        
+        JPanel panel = new JPanel();
+        
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
+        
+        p1.setLayout(new BoxLayout(p1,BoxLayout.Y_AXIS));
+        p2.setLayout(new BoxLayout(p2,BoxLayout.Y_AXIS));
+        
+        Lab nom = new Lab("Nom : " + enseignant.getNom());
+        Lab prenom = new Lab("Prénom : " + enseignant.getPrenom());
+        
+        p1.add(nom);
+        p2.add(prenom);
+        
+        panel.add(p1);
+        panel.add(p2);
+        
+        panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+    }
+}
+
